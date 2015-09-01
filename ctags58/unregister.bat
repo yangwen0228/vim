ff93ddeacef32b17598bc749ed4ecce4c7e82a40
yangwen0228 <yangwen0228@foxmail.com>
2015-09-01 17:46:28 +0800

    remove all

--- ctags58/unregister.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-echo Windows Registry Editor Version 5.00> register.reg
-echo.>>register.reg
-echo [-HKEY_LOCAL_MACHINE\SOFTWARE\ctags58\ctags]>> register.reg
-echo.>>register.reg
-echo [-HKEY_LOCAL_MACHINE\SOFTWARE\ctags58]>> register.reg
-regedit /s register.reg
-if exist register.reg del register.reg
